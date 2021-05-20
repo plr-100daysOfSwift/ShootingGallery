10c406a31a0aa99ad2f6cb069f0df350c7fdde4f
Paul Richardson <developer@photographerafoot.com>
2021-05-21 09:02:51 +1000

    CHG: Ignore touches if timeRemaining == 0

--- ShootingGallery/GameScene.swift
+++ ShootingGallery/GameScene.swift
@@ -94,6 +94,7 @@ class GameScene: SKScene {
 
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		guard let touch = touches.first else { return }
+		guard  timeRemaining > 0 else { return }
 
 		if shotsRemaining > 0 {
 			shotsRemaining -= 1
